24c67ebacc65f895debf15ffe9bd96121fd468c1
SimonNjoroge <simonwangui0038@gmail.com>
2023-11-26 20:33:56 +0300

    menu fix

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
-const btn = document.getElementById('menu-btn')
-const nav = document.getElementById('menu')
+const btn = document.getElementById('menu-btn');
+const nav = document.getElementById('menu');
 
+// Function to hide the menu
+function hideMenu() {
+   btn.classList.remove('open');
+   nav.classList.remove('flex');
+   nav.classList.add('hidden');
+}
+
+// Event listener for the menu button click
 btn.addEventListener('click', () => {
-   btn.classList.toggle('open')
-   nav.classList.toggle('flex')
-   nav.classList.toggle('hidden')
- })
- 
+   btn.classList.toggle('open');
+   nav.classList.toggle('flex');
+   nav.classList.toggle('hidden');
+});
+
+// Event listener for the scroll event on the window
+window.addEventListener('scroll', () => {
+   // Call the hideMenu function when the user scrolls
+   hideMenu();
+});
